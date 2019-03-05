f999488619a64a8d249dd2e86afcbeec41cb6ef3
Emrah Öz <emr550maranello@gmail.com>
2019-03-05 22:35:29 +0300

    mongo db update

--- index.js
+++ index.js
@@ -4,96 +4,71 @@ var mongoSanitize = require('express-mongo-sanitize');
 var db = require("./db/mongo");
 var log = require("./logger");
 var sessionUtil = require("./utility/session");
-var port = process.env.PORT || 1337;
+var port = process.env.PORT || 8080;
 
+db.OpenDB().then((result) => {
+    console.log("DB Connection OK: " + result);
 
-var app = express();
-db.OpenDB();
-app.use(bodyParser.json({ limit: '50mb' }));
-app.use(mongoSanitize({ replaceWith: '_' }));
-
-
-
-app.get("/",function(req,res){
-
-    res.send("Hello World from My Code Test");
-
-});
-
-app.use(function (req, res, next) {
-    var send = res.send;
-    res.send = function (data) {
-         log.info("Response:" + data + "\n\n");
-        send.call(this, data);
-    };
-    log.info("Request" + JSON.stringify(req.body));
-    next();
-});
-
-
-
-app.use(["/api/open", "/api/open*"], function (request, response, next) {
-    var requestRedirect = require("./api/open" + request.url);
-
-    requestRedirect(request, response).then(function (result) {
-        response.json(result);
+    const app = express()
+    app.use(bodyParser.json());
+    app.use(mongoSanitize({ replaceWith: '_' }));
+    
+    //request response logger middleware
+    app.use(function (req, res, next) {
+        var send = res.send;
+        res.send = function (data) {
+            console.log("Response:" + data + "\n\n");
+            send.call(this, data);
+        };
+        console.log("Request" + JSON.stringify(req.body));
         next();
-    }).catch(function (err) {
-        next({ body: err, reqBody: request.body, message: "Error" });
     });
-});
+    app.post(["/api/open", "/api/open*"], function (request, response, next) {
 
-app.use(["/api/auth", "/api/auth*"], function (request, response, next) {
-
-    var requestRedirect = require("./api/auth" + request.url);
-    var { username, session } = request.body;
-    if (username && session) {
-        sessionUtil.checksession(session, username).then(function (returnValue) {
-
-
-            requestRedirect(request, response).then(function (result) {
-                response.json(result);
-                next();
-            }).catch(function (err) {
-                next({ body: err, reqBody: request.body, message: "Error" });
-            });
+        var requestRedirect = require("./api/open" + request.url.replace('/api/open', ''));
+        requestRedirect(request, response).then(function (result) {
+            response.json(result);
+            next();
         }).catch(function (err) {
-            next({ body: err, reqBody: request.body, message: "Error" });
-        });
-    } else {
-        var result = {
-            success: false,
-            message: "Auth Error"
+            response.json(err);
+            next();
+        })
+    }); 
+    app.post(["/api/auth", "/api/auth*"], function (request, response, next) {
+
+        var requestRedirect = require("./api/auth" + request.url.replace('/api/auth', ''));
+        var { username, session } = request.body;
+        if (username && session) {
+            sessionUtil.checksession(session , username ).then(() => {
+                requestRedirect(request, response).then(function (result) {
+                    response.json(result);
+                    next();
+                }).catch(function (err) {
+                    response.json(err);
+                    next();
+                })
+            }).catch(function (err) {
+                response.json(err);
+                next();
+            });
+        } else {
+            response.json({ result: false, message: "Incorrect session." });
+            next();
         }
-        next({ body: result, reqBody: request.body, message: "Error" });
-    }
-})
+    });
 
-app.use(function (err, req, res, next) {
-    var result = {
-        success: false,
-        message: 'Api Error'
-    }
-    if (err.body)
-        result = err.body;
+    //General Error Logger..
+    app.use(function (err, req, res, next) {
+        if (err) {
+            console.log("General Error:");
+            console.log(err.stack);
+        }
+        res.json({ message: "Global Error Occurred" });
+        next();
+    });
+ 
+    app.listen(port, () => console.log("Server Ready On port 8080"));
 
-    log.error("ERROR_START:");
-    log.error("PATH:\t" + req.path);
-    if (err.reqBody) {
-        log.error("REQUEST:\n" + JSON.stringify(err.reqBody));
-    }
-    if (err.body) {
-        log.error("RESPONSE:\n" + JSON.stringify(err.body));
-    } else {
-        log.error("RESPONSE:\n" + err);
-    }
-    log.error("TITLE:\t" + err.message);
-    if (err.stack)
-        log.error("STACK TRACE:\n" + err.stack);
-    log.error("ERROR_END:\n\n");
-
-    res.json(result);
-})
-
-
-app.listen(port);
+}).catch((err) => {
+    console.log("DB Connection Not OK!: " + err);
+});
